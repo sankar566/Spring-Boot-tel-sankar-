df6bb6b760b13f740e001526cd87409c476b22df
sankar566 <60536375+sankar566@users.noreply.github.com>
2020-12-29 11:48:13 +0530

    Add files via upload

---
+++ springboot.tel5.object.MVC.Sankar/src/main/java/com/Tel5/MVC/Sankar/Application.java
@@ -0,0 +1,13 @@
+package com.Tel5.MVC.Sankar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
