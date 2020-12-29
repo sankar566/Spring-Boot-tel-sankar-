0626d6b99f96ff335bea36add55c7cebf56b690d
sankar566 <60536375+sankar566@users.noreply.github.com>
2020-12-29 11:43:18 +0530

    Add files via upload

---
+++ springboot.Tel4.Web.object.MVC.sankar/src/main/java/com/Tel4/web/sankar/HomeController.java
@@ -0,0 +1,99 @@
+package com.Tel4.web.sankar;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("home")
+	public void home() {
+		System.out.println("This is home method ");
+	}
+	
+	@RequestMapping("home1")
+	@ResponseBody
+	public String home1() {
+		System.out.println("This is home1 method ");
+		return "Home.jsp";
+	}
+//	jsp file is placed webapp folder inside 
+	    @RequestMapping("home2")
+		public String home2() {
+		System.out.println("This is home2 method ");
+		return "Home.jsp";
+	}
+	    @RequestMapping("home4")
+		public String home4(HttpServletRequest req) {
+	    String name = req.getParameter("dog");
+		System.out.println("This is home4 method ");
+		System.out.println("hai" +name);
+		return "Home.jsp";
+	}
+	    @RequestMapping("home5")
+		public String home5(HttpServletRequest req) {
+	    HttpSession session = req.getSession();
+	    String name = req.getParameter("dog");
+	    session.setAttribute("keyname", name);
+		System.out.println("This is home 5 method ");
+		System.out.println("hai" +name);
+		return "Home.jsp";
+	}
+	    @RequestMapping("home6")
+		public String home6(HttpServletRequest req,HttpServletResponse res) {
+	    HttpSession session = req.getSession();
+	    String name = req.getParameter("dog");
+	    session.setAttribute("keyname6", name);
+		System.out.println("This is home 6 method ");
+		System.out.println("hai" +name);
+		return "Home.jsp";
+	}
+	    @RequestMapping("home7")
+		public String home7(String name7,HttpSession session) {
+	    
+	    session.setAttribute("keyname7", name7);
+		System.out.println("This is home 7 method ");
+		System.out.println("hai" +name7);
+		return "Home.jsp";
+	}
+	    @RequestMapping("home8")
+		public String home8(String myname,HttpSession session) {
+	    
+	    session.setAttribute("keyname8", myname);
+		System.out.println("This is home 8 method ");
+		System.out.println("hai" +myname);
+		return "Home.jsp";
+	}
+	    @RequestMapping("home9")
+		public String home9(@RequestParam("anyname") String myname9,HttpSession session) {
+	    
+	    session.setAttribute("keyname9", myname9);
+		System.out.println("This is home 9 method ");
+		System.out.println("hai" +myname9);
+		return "Home.jsp";
+	}
+	    @RequestMapping("home10")
+		public ModelAndView home10(@RequestParam("anyname") String myname10) {
+	    ModelAndView mv = new ModelAndView();
+	    mv.addObject("keyname10", myname10);
+	    mv.setViewName("Home.jsp");
+		return mv;
+	}
+	    @RequestMapping("home11")
+		public ModelAndView home11(Alien alien) {
+	    ModelAndView mv = new ModelAndView();
+	    mv.addObject("keyobject", alien);
+	    mv.setViewName("Home.jsp");
+		return mv;
+	}
+
+
+}
---
+++ springboot.Tel4.Web.object.MVC.sankar/src/main/webapp/Home.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+ <h1>This is home jsp page </h1>
+ <h1> (home6 method) My name is ${keyname6}</h1>
+  <h1>(home7 method) My name is  ${keyname7}</h1>
+  <h1>(home8 method) My name is  ${keyname8}</h1>
+   <h1>(home9 method) My name is  ${keyname9}</h1>
+   <h1>(home10 method) ModelandView ----- My name is  ${keyname10}</h1>
+   <h1>(home11 method) ModelandView ---object pass as parameter-- My name is  ${keyobject.aid},${keyobject.aname},${keyobject.lang}</h1>
+  
+</body>
+</html>
